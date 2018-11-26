7d2a5c6fd1906ef0670516b3d46332e62a958070
Luke Kozak <kozakluke@gmail.com>
2018-11-26 15:04:05 +0100

    Init

---
+++ index.ts
@@ -0,0 +1,13 @@
+import BezierUtil from './src/BezierUtil';
+import Cursor from './src/Cursor';
+import MathUtil from './src/MathUtil';
+import Pool from './src/Pool';
+import range from './src/range';
+import String from './src/StringUtil';
+import classNames from './src/classNames';
+import DateUtil from './src/DateUtil';
+/**
+ * @author kozakluke@gmail.com
+ */
+console.log(DateUtil.getMonthName(new Date()));
+console.log(DateUtil.getMonthShortName(new Date()));
