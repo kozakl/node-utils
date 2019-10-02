258a900cc0809496c905557f9faef6520d1c18d3
Luke Kozak <kozakluke@gmail.com>
2019-10-02 23:32:28 +0200

    Create makeImageSet function in image util

--- src/image.ts
+++ src/image.ts
@@ -1,3 +1,5 @@
+import {basename, extname, join} from 'path';
+import {existsSync, mkdirpSync} from 'fs-extra';
 import gm from 'gm';
 
 export function imageSize(image:string) {
@@ -8,4 +10,49 @@ export function imageSize(image:string) {
                                 reject(error)
         });
     });
+}
+
+export function makeImageSet(image:ImageSet)
+{
+    if (!existsSync(image.dest)) {
+        mkdirpSync(image.dest);
+    }
+    
+    image.sizes.forEach((size)=> {
+        const imageName = basename(image.src, extname(image.src));
+        if (image.destDir)
+            mkdirpSync(join(image.dest, imageName));
+        
+        const dest = join(
+            image.dest,
+            image.destDir ? imageName : '' ,
+            (size.name || imageName) +
+            (size.suffix || '') +
+            (image.format || extname(image.src))
+        );
+        gm(image.src)
+            .noProfile()
+            .resize(size.value)
+            .blur(size.blur[0] || image.blur[0] || 0,
+                  size.blur[1] || image.blur[1] || 0)
+            .quality(size.quality || image.quality)
+            .write(dest, (error)=>
+                error && console.log(error));
+    });
+}
+
+interface ImageSet {
+    src:string;
+    dest:string;
+    destDir:boolean;
+    format:string;
+    quality:number;
+    blur:number[];
+    sizes: {
+        value:number;
+        name:string;
+        suffix:string;
+        quality:number;
+        blur:number[];
+    }[]
 }
