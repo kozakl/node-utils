cf4a3f421841e170f99886df3bcdd2d198f96556
Luke Kozak <kozakluke@gmail.com>
2019-10-20 17:32:19 +0200

    Update makeImageSet to async and add debug mode

--- src/image.ts
+++ src/image.ts
@@ -12,33 +12,55 @@ export function imageSize(image:string) {
     });
 }
 
-export function makeImageSet(image:ImageSet)
+export async function makeImageSet(image:ImageSet, debug = false)
 {
     if (!existsSync(image.dest)) {
         mkdirpSync(image.dest);
     }
     
-    image.sizes.forEach((size)=> {
-        const imageName = basename(image.src, extname(image.src));
-        if (image.destDir)
-            mkdirpSync(join(image.dest, imageName));
-        
-        const dest = join(
-            image.dest,
-            image.destDir ? imageName : '' ,
-            (size.name || imageName) +
-            (size.suffix || '') +
-            (image.format || extname(image.src))
-        );
-        gm(image.src)
-            .noProfile()
-            .resize(size.value)
-            .blur(size.blur && size.blur[0] || 0,
-                  size.blur && size.blur[1])
-            .quality(size.quality || image.quality)
-            .write(dest, (error)=>
-                error && console.log(error));
-    });
+    console.log(image.src);
+    await image.sizes.reduce(async (promise, size)=> {
+        await promise;
+        await new Promise((resolve, reject)=> {
+            const name = basename(image.src, extname(image.src));
+            const dest = join(
+                image.dest,
+                image.destDir ? name : '' ,
+                (size.name || name) +
+                (size.suffix || '') +
+                (image.format || extname(image.src))
+            );
+            if (image.destDir) {
+                mkdirpSync(join(image.dest, name));
+            }
+            if (!debug) {
+                gm(image.src)
+                    .noProfile()
+                    .resize(size.value)
+                    .blur(0, size.blur || 0.001)
+                    .quality(size.quality || image.quality)
+                    .write(dest, (error)=>
+                        !error ? resolve() :
+                                 reject(error));
+            } else {
+                gm(image.src)
+                    .noProfile()
+                    .resize(size.value)
+                    .blur(0, size.blur || 0.001)
+                    .quality(size.quality || image.quality)
+                    .fill('#FFFFFF')
+                    .font('Helvetica', 0.1 * size.value)
+                    .drawText(
+                        0.4 * size.value,
+                        0.11 * size.value,
+                        size.name
+                    )
+                    .write(dest, (error)=>
+                        !error ? resolve() :
+                                 reject(error));
+            }
+        });
+    }, Promise.resolve());
 }
 
 interface ImageSet {
@@ -52,6 +74,6 @@ interface ImageSet {
         name:string;
         suffix:string;
         quality:number;
-        blur:number[];
+        blur:number;
     }[]
 }
